fb6b4f7b5614ba1dcd4446207620ec54a96089e4
João Peres <114088733+joaodperes@users.noreply.github.com>
2026-01-28 16:33:24 +0000

    Cleaned css and html

--- src/gameData.js
+++ src/gameData.js
@@ -32,14 +32,14 @@ export let gameData = {
         maxEnergy: 0 
     },
     buildings: {
-        mine: createBuilding("Metal Mine", "Produces metal", 60, 15, 0, 60, 10, 2),
-        crystal: createBuilding("Crystal Drill", "Produces crystal", 48, 24, 0, 30, 15, 1),
-        deuterium: createBuilding("Deuterium Synthesizer", "Produces deuterium", 225, 75, 0, 10, 25, 3, {
+        mine: createBuilding("Metal Mine", "Produces metal", 60, 15, 0, 60, 10, 2, "/h"),
+        crystal: createBuilding("Crystal Drill", "Produces crystal", 48, 24, 0, 30, 15, 1, "/h"),
+        deuterium: createBuilding("Deuterium Synthesizer", "Produces deuterium", 225, 75, 0, 10, 25, 3, "/h", {
             mine: { 1: 5, 5 : 10 }, // Lvl 1: requires Mine Lvl 5; Lvl 5: requires Mine Lvl 10
             crystal: { 2 : 2} 
         }),
         solar: createBuilding("Solar Plant", "Produces energy", 75, 30, 0, 0, 20, -20,"⚡"), //negative energyWeight means production
-        robotics: createBuilding("Robotics Factory", "Reduces construction time", 400, 120, 0, 0, 120, 0,"% Time", {
+        robotics: createBuilding("Robotics Factory", "Reduces construction time", 400, 200, 100, 0, 120, 0,"% Time", {
                 mine: { 1 : 10 },
                 solar: { 10 : 30},
                 deuterium: { 10 : 15 }
